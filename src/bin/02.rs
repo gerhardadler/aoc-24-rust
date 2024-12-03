399720b6d93e913fc2d71b8daf4bb021e4144fd8
gerhardadler <gerhardskartveit@gmail.com>
2024-12-03 08:51:51 +0100

    now skips correct numbers when errors at start

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -45,16 +45,16 @@ fn is_safe_two<I>(numbers: I, skipped_index: Option<usize>) -> bool
 where
     I: Iterator<Item = i32> + Clone,
 {
-    let mut work_numbers = numbers.clone();
+    let mut work_numbers = numbers.clone().enumerate();
 
     if skipped_index == Some(0) {
         work_numbers.next();
     }
 
-    let mut last_number = work_numbers.next().unwrap();
+    let mut last_number = work_numbers.next().unwrap().1;
     let mut direction = None;
 
-    for (i, number) in work_numbers.enumerate() {
+    for (i, number) in work_numbers {
         if skipped_index == Some(i) {
             continue;
         }
@@ -66,7 +66,9 @@ where
             if skipped_index.is_some() {
                 return false;
             }
-            if i == 1 && is_safe_two(numbers.clone(), Some(0)) {
+            if i == 2
+                && (is_safe_two(numbers.clone(), Some(0)) || is_safe_two(numbers.clone(), Some(1)))
+            {
                 return true;
             }
             return is_safe_two(numbers.clone(), Some(i));
@@ -105,6 +107,29 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_is_safe_two() {
+        let input = "1 2 3 4 5";
+        let split_line = input.split(' ');
+        let numbers = split_line.map(|s| s.parse::<i32>().unwrap());
+        assert_eq!(is_safe_two(numbers, None), true);
+
+        let input = "10 8 11 12 14";
+        let split_line = input.split(' ');
+        let numbers = split_line.map(|s| s.parse::<i32>().unwrap());
+        assert_eq!(is_safe_two(numbers, None), true);
+
+        let input = "10 8 8 8 14";
+        let split_line = input.split(' ');
+        let numbers = split_line.map(|s| s.parse::<i32>().unwrap());
+        assert_eq!(is_safe_two(numbers, None), false);
+
+        let input = "20 11 8 7 6";
+        let split_line = input.split(' ');
+        let numbers = split_line.map(|s| s.parse::<i32>().unwrap());
+        assert_eq!(is_safe_two(numbers, None), false);
     }
 
     #[test]
